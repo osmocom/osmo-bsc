osmocom/osmo-bsc

commit d382bf63e2b7e28fe41c5310c26fe584f0356897

paging: Stop all paging if MSC sends us BSSMAP RESET
When the MSC has lost its state and issues a RESET, we should not only
clear all ongoing radio connections, but we should also stop any paging.
There's no point in paging a subscriber if the MSC doesn't know about
this paging anymore.

Change-Id: If3f53d3bb66ad2dc02db823cb813590c6b59c700
Closes: OS#2736

src/libbsc/paging.c
@@ -467,3 +467,26 @@ struct bsc_msc_data *paging_get_msc(struct gsm_bts *bts, struct bsc_subscr *bsub
 
 	return NULL;
 }
+
+/*! Flush all paging requests at a given BTS for a given MSC*/
+void paging_flush_bts(struct gsm_bts *bts, struct bsc_msc_data *msc)
+{
+	struct gsm_paging_request *req, *req2;
+
+	llist_for_each_entry_safe(req, req2, &bts->paging.pending_requests, entry) {
+		if (msc && req->msc != msc)
+			continue;
+		/* now give up the data structure */
+		LOGP(DPAG, LOGL_DEBUG, "Stop paging %s on bts %d (flush).\n", req->bsub->imsi, bts->nr);
+		paging_remove_request(&bts->paging, req);
+	}
+}
+
+/*! Flush all paging requests issued by \a msc on any BTS in \a net */
+void paging_flush_network(struct gsm_network *net, struct bsc_msc_data *msc)
+{
+	struct gsm_bts *bts;
+
+	llist_for_each_entry(bts, &net->bts_list, list)
+		paging_flush_bts(bts, msc);
+}

include/osmocom/bsc/paging.h
@@ -75,4 +75,7 @@ unsigned int paging_pending_requests_nr(struct gsm_bts *bts);
 
 struct bsc_msc_data *paging_get_msc(struct gsm_bts *bts, struct bsc_subscr *bsub);
 
+void paging_flush_bts(struct gsm_bts *bts, struct bsc_msc_data *msc);
+void paging_flush_network(struct gsm_network *net, struct bsc_msc_data *msc);
+
 #endif

src/osmo-bsc/osmo_bsc_bssap.c
@@ -218,6 +218,9 @@ static int bssmap_handle_reset(struct bsc_msc_data *msc,
 	 * close all active channels on the BTS side as well */
 	osmo_bsc_sigtran_reset(msc);
 
+	/* Drop all ongoing paging requests that this MSC has created on any BTS */
+	paging_flush_network(msc->network, msc);
+
 	/* Inform the MSC that we have received the reset request and
 	 * that we acted accordingly */
 	osmo_bsc_sigtran_tx_reset_ack(msc);

commit 719322693c2803803326a909a9d3e57564ad7236

fix segfault upon release paging on BSSMAP Reset: init llist
Initialize the llist head gsm_bts->paging.pending_requests at the time gsm_bts
is allocated, not only at paging_init_if_needed().

The gsm_bts->paging sub-struct is invalid as long as gsm_bts->paging.bts
doesn't point back to bts. Hence the recently added iteration of
gsm_bts->paging.pending_requests should have checked whether bts is NULL. The
llist_head pending_requests is not initialized unless paging_init_if_needed()
has been called (and paging.bts is hence set). However, this fix is a safer way
to prevent errors like this in general.

The segfault was introduced by d382bf63e2b7e28fe41c5310c26fe584f0356897 /
If3f53d3bb66ad2dc02db823cb813590c6b59c700

Related: OS#2747
Change-Id: Idfafac4e2c0e0a241a62aecbbdc22be71febf840

src/libbsc/paging.c
@@ -240,7 +240,11 @@ static void paging_init_if_needed(struct gsm_bts *bts)
 		return;
 
 	bts->paging.bts = bts;
-	INIT_LLIST_HEAD(&bts->paging.pending_requests);
+
+	/* This should be initialized only once. There is currently no code that sets bts->paging.bts
+	 * back to NULL, so let's just assert this one instead of graceful handling. */
+	OSMO_ASSERT(llist_empty(&bts->paging.pending_requests));
+
 	osmo_timer_setup(&bts->paging.work_timer, paging_worker,
 			 &bts->paging);
 

src/libcommon/gsm_data_shared.c
@@ -364,7 +364,10 @@ struct gsm_bts *gsm_bts_alloc(struct gsm_network *net, uint8_t bts_num)
 
 	bts->rach_b_thresh = -1;
 	bts->rach_ldavg_slots = -1;
+
 	bts->paging.free_chans_need = -1;
+	INIT_LLIST_HEAD(&bts->paging.pending_requests);
+
 	bts->features.data = &bts->_features_data[0];
 	bts->features.data_len = sizeof(bts->_features_data);
 