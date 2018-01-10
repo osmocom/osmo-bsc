6b103e14ba8895cd58968c1eee90f43e0f4b85c9
Vadim Yanitskiy <axilirator@gmail.com>
2018-01-10 18:03:09 +0600

    bsc/gsm_04_80.h: clean up useless declarations
    This change removes a few USSD specific declarations, which are
    not actually used now, and probably accidentally migrated from
    legacy OpenBSC.
    
    Change-Id: I060223349971a7fce169a018bf16eb91f0e20342

--- include/osmocom/bsc/gsm_04_80.h
+++ include/osmocom/bsc/gsm_04_80.h
@@ -1,21 +1,6 @@
 #pragma once
 
-#include <osmocom/core/msgb.h>
-#include <osmocom/gsm/protocol/gsm_04_80.h>
-#include <osmocom/gsm/gsm0480.h>
-
 struct gsm_subscriber_connection;
-
-int gsm0480_send_ussd_response(struct gsm_subscriber_connection *conn,
-			       const struct msgb *in_msg, const char* response_text, 
-			       const struct ss_request *req);
-int gsm0480_send_ussd_reject(struct gsm_subscriber_connection *conn,
-			     const struct msgb *msg, 
-			     const struct ss_request *request);
-
-int msc_send_ussd_notify(struct gsm_subscriber_connection *conn, int level,
-			 const char *text);
-int msc_send_ussd_release_complete(struct gsm_subscriber_connection *conn);
 
 int bsc_send_ussd_notify(struct gsm_subscriber_connection *conn, int level,
 			 const char *text);
