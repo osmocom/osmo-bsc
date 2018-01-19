osmocom/openbsc

HO: Count the actual meas.rep. get_meas_rep_avg fails if not reached
get_meas_rep_avg will return -EINVAL, if the required number of
measurements are not reached. There will be no handover possible until the
given number of measurements are available.

openbsc/src/libbsc/chan_alloc.c
@@ -323,6 +323,10 @@ struct gsm_lchan *lchan_alloc(struct gsm_bts *bts, enum gsm_chan_t type,
 	if (lchan) {
 		lchan->type = type;
 
+		/* reset measuement report counter and index */
+	        lchan->meas_rep_cnt = 0;
+	        lchan->meas_rep_idx = 0;
+
 		/* clear sapis */
 		memset(lchan->sapis, 0, ARRAY_SIZE(lchan->sapis));
 

osmocom/osmo-bsc

Change-Id: Ibc4410b4e162cdb6c070128d2c63946bb79d6d65

src/libbsc/chan_alloc.c
@@ -347,6 +347,10 @@ struct gsm_lchan *lchan_alloc(struct gsm_bts *bts, enum gsm_chan_t type,
 		     gsm_ts_and_pchan_name(lchan->ts),
 		     lchan->nr, gsm_lchant_name(lchan->type));
 
+		/* reset measurement report counter and index */
+	        lchan->meas_rep_count = 0;
+	        lchan->meas_rep_idx = 0;
+
 		/* clear sapis */
 		memset(lchan->sapis, 0, ARRAY_SIZE(lchan->sapis));
 