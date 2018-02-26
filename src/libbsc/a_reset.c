3d5818a90190081bb1f7e0f56b9ba7cbcf6bd2d9
Philipp Maier <pmaier@sysmocom.de>
2018-02-26 21:47:06 +0100

    a_reset: Add FSM event names
    The FSM lacks a proper definition of the FSM event names. This
    causes problems when inspecting the FSM using the VTY.
    
    - Add proper FSM Event names
    
    Change-Id: Ief1eff45a896d6191bdc64c232be69e85bfc63dc
    Related: OS#2924

--- src/libbsc/a_reset.c
+++ src/libbsc/a_reset.c
@@ -44,6 +44,13 @@ enum fsm_evt {
 	EV_N_CONNECT,		/* made a successful connection */
 };
 
+static const struct value_string fsm_event_names[] = {
+	OSMO_VALUE_STRING(EV_RESET_ACK),
+	OSMO_VALUE_STRING(EV_N_DISCONNECT),
+	OSMO_VALUE_STRING(EV_N_CONNECT),
+	{0, NULL}
+};
+
 /* Disconnected state */
 static void fsm_disc_cb(struct osmo_fsm_inst *fi, uint32_t event, void *data)
 {
@@ -112,6 +119,7 @@ static struct osmo_fsm fsm = {
 	.num_states = ARRAY_SIZE(fsm_states),
 	.log_subsys = DMSC,
 	.timer_cb = fsm_reset_ack_timeout_cb,
+	.event_names = fsm_event_names,
 };
 
 /* Create and start state machine which handles the reset/reset-ack procedure */
