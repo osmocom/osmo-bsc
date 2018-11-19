osmocom/osmo-bsc

commit 65c62e50335b500ac9e4658530ca5a1b4f5328d8

check for overlong unix socket paths
In rf_create_socket(), check the result of osmo_strlcpy().
This catches overlong and non-NUL-terminated socket paths.

Change-Id: I4b9ff9146068bf7f53d67c577454f32c02361b86
Related: OS#2673

src/osmo-bsc/bsc_rf_ctrl.c
@@ -450,8 +450,12 @@ static int rf_create_socket(struct osmo_bsc_rf *rf, const char *path)
 	}
 
 	local.sun_family = AF_UNIX;
-	osmo_strlcpy(local.sun_path, path, sizeof(local.sun_path));
 	unlink(local.sun_path);
+	if (osmo_strlcpy(local.sun_path, path, sizeof(local.sun_path)) >= sizeof(local.sun_path)) {
+		LOGP(DLINP, LOGL_ERROR, "Socket path exceeds maximum length of %zd bytes: %s\n",
+		     sizeof(local.sun_path), path);
+		return -1;
+	}
 
 	/* we use the same magic that X11 uses in Xtranssock.c for
 	 * calculating the proper length of the sockaddr */

commit 726b097b0c1a6042186736ffc18d4666a609453b

unlink socket path correctly in rf_create_socket()
In commit 65c62e50335b500ac9e4658530ca5a1b4f5328d8 a call
to unlink() was erroneously moved up. Since then unlink()
has been called with an uninitialized path variable. The
problem went unnoticed because the return value of unlink()
was never checked.

Ensure that unlink() is called with an initialized argument and
verify success of the unlink() operation if the socket exists.

Related: CID#188836
Change-Id: Ia0c873da305cbb47aef0562f61ec21057363f294
Fixes: 65c62e50335b500ac9e4658530ca5a1b4f5328d8

src/osmo-bsc/bsc_rf_ctrl.c
@@ -450,10 +450,14 @@ static int rf_create_socket(struct osmo_bsc_rf *rf, const char *path)
 	}
 
 	local.sun_family = AF_UNIX;
-	unlink(local.sun_path);
 	if (osmo_strlcpy(local.sun_path, path, sizeof(local.sun_path)) >= sizeof(local.sun_path)) {
 		LOGP(DLINP, LOGL_ERROR, "Socket path exceeds maximum length of %zd bytes: %s\n",
 		     sizeof(local.sun_path), path);
+		return -1;
+	}
+	if (unlink(local.sun_path) < 0 && errno != ENOENT) {
+		LOGP(DLINP, LOGL_ERROR, "Could not unlink socket path %s: %d/%s\n",
+		     path, errno, strerror(errno));
 		return -1;
 	}
 