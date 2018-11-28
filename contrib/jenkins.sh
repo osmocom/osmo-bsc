ab9a259cd2e5f0d188ebfecdb2a271e8c0dba94b
Oliver Smith <osmith@sysmocom.de>
2018-11-28 11:33:39 +0100

    contrib/jenkins.sh: build and publish manuals
    Add new environment variables WITH_MANUALS and PUBLISH to control if
    the manuals should be built and uploaded. Describe all environment vars
    on top of the file.
    
    When WITH_MANUALS is set, install osmo-gsm-manuals like any other
    dependency and add --enable-manuals to the configure flags (for "make"
    and "make distcheck"). Add the bin subdir of the installed files to
    PATH, so osmo-gsm-manuals-check-depends can be used by ./configure.
    
    Related: OS#3385
    Change-Id: Ief6ce94013612a968183e82abef421f116ed37c3

--- contrib/jenkins.sh
+++ contrib/jenkins.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 # jenkins build helper script for openbsc.  This is how we build on jenkins.osmocom.org
+#
+# environment variables:
+# * WITH_MANUALS: build manual PDFs if set to "1"
+# * PUBLISH: upload manuals after building if set to "1" (ignored without WITH_MANUALS = "1")
+#
 
 if ! [ -x "$(command -v osmo-build-dep.sh)" ]; then
 	echo "Error: We need to have scripts/osmo-deps.sh from http://git.osmocom.org/osmo-ci/ in PATH !"
@@ -24,11 +29,19 @@ verify_value_string_arrays_are_terminated.py $(find . -name "*.[hc]")
 
 export PKG_CONFIG_PATH="$inst/lib/pkgconfig:$PKG_CONFIG_PATH"
 export LD_LIBRARY_PATH="$inst/lib"
+export PATH="$inst/bin:$PATH"
 
 osmo-build-dep.sh libosmo-abis
 osmo-build-dep.sh libosmo-netif
 osmo-build-dep.sh libosmo-sccp
 osmo-build-dep.sh osmo-mgw
+
+# Additional configure options and depends
+CONFIG=""
+if [ "$WITH_MANUALS" = "1" ]; then
+	osmo-build-dep.sh osmo-gsm-manuals
+	CONFIG="--enable-manuals"
+fi
 
 set +x
 echo
@@ -40,13 +53,17 @@ set -x
 
 cd "$base"
 autoreconf --install --force
-./configure --enable-sanitize --enable-external-tests --enable-werror
+./configure --enable-sanitize --enable-external-tests --enable-werror $CONFIG
 $MAKE $PARALLEL_MAKE
 LD_LIBRARY_PATH="$inst/lib" $MAKE check \
   || cat-testlogs.sh
 LD_LIBRARY_PATH="$inst/lib" \
-  DISTCHECK_CONFIGURE_FLAGS="--enable-vty-tests --enable-external-tests --enable-werror" \
+  DISTCHECK_CONFIGURE_FLAGS="--enable-vty-tests --enable-external-tests --enable-werror $CONFIG" \
   $MAKE distcheck \
   || cat-testlogs.sh
+
+if [ "$WITH_MANUALS" = "1" ] && [ "$PUBLISH" = "1" ]; then
+	make -C "$base/doc/manuals" publish
+fi
 
 osmo-clean-workspace.sh
