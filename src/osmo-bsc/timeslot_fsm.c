ba4f66a31d17a5a99faf614301bf6dd53f065f23
Pau Espin Pedrol <pespin@sysmocom.de>
2018-11-28 17:31:41 +0100

    bsc: timeslot_fsm: Fix count in ts_lchans_waiting
    Before this patch, TCH lchans waiting for dynamic TS to
    switch PDCH->TCH wouldn't be counted.
    
    See osmo-bsc I9cedb77d6578597f1febab36c54b2ee427c7a4a2 for similar
    extensive explanation.
    
    Change-Id: I32008859cc23cd2afddd79daae21497d0945fed0

--- src/osmo-bsc/timeslot_fsm.c
+++ src/osmo-bsc/timeslot_fsm.c
@@ -144,7 +144,7 @@ static int ts_lchans_waiting(struct gsm_bts_trx_ts *ts)
 {
 	struct gsm_lchan *lchan;
 	int count = 0;
-	ts_for_each_lchan(lchan, ts)
+	ts_for_each_potential_lchan(lchan, ts)
 		if (lchan->fi->state == LCHAN_ST_WAIT_TS_READY)
 			count++;
 	return count;
