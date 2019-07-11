osmocom/osmo-bsc

commit 471fd92170297f1c857bb448bbf21a162c2ed655

contrib/jenkins.sh: run "make maintainer-clean"
Related: OS#3047
Change-Id: I5c257e21376cdccd6e2f413c7df6dd8caef497f1

contrib/jenkins.sh
@@ -61,6 +61,7 @@ LD_LIBRARY_PATH="$inst/lib" \
   DISTCHECK_CONFIGURE_FLAGS="--enable-vty-tests --enable-external-tests --enable-werror $CONFIG" \
   $MAKE distcheck \
   || cat-testlogs.sh
+$MAKE maintainer-clean
 
 if [ "$WITH_MANUALS" = "1" ] && [ "$PUBLISH" = "1" ]; then
 	make -C "$base/doc/manuals" publish

commit 14c80fdb65671615cbb44329bae9f57a4e6d68d7

contrib/jenkins.sh: "maintainer-clean" after "publish"
Run "make maintainer-clean" after publishing manuals, not the other way
around. Otherwise jenkins.sh fails when running for the master branch,
because docs/manuals/Makefile gets deleted although it is still needed
to publish the manuals.

Related: OS#3047
Fixes: 471fd92170297f1c857bb448bbf21a162c2ed655 ("contrib/jenkins.sh: run "make maintainer-clean"")
Change-Id: I8ba5369b0948b61c68f43d807312c52465119aa5

contrib/jenkins.sh
@@ -61,10 +61,10 @@ LD_LIBRARY_PATH="$inst/lib" \
   DISTCHECK_CONFIGURE_FLAGS="--enable-vty-tests --enable-external-tests --enable-werror $CONFIG" \
   $MAKE distcheck \
   || cat-testlogs.sh
-$MAKE maintainer-clean
 
 if [ "$WITH_MANUALS" = "1" ] && [ "$PUBLISH" = "1" ]; then
 	make -C "$base/doc/manuals" publish
 fi
 
+$MAKE maintainer-clean
 osmo-clean-workspace.sh