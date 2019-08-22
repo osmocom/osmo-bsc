e1b6a16924b55f532699792afdfb0048a2d61b17
Neels Hofmeyr <neels@hofmeyr.de>
2019-08-22 21:46:44 +0200

    lchan_fsm: allow transition from broken state to release
    If a channel activation ack comes in late from a BTS, the borken state has a
    transition back to a channel recovery. This only fails because of a missing
    state transition in the lchan_fsm. Allow the transition.
    
    We've seen this happening at cccamp2019, where a particular BTS for unknown
    reasons sends its Chan Act ACK a second after the activation times out in
    osmo-bsc. This patch should allow it to recover.
    
    Change-Id: I73139033ae44abb383ccce956c1c9683db8c8816

--- src/osmo-bsc/lchan_fsm.c
+++ src/osmo-bsc/lchan_fsm.c
@@ -1268,6 +1268,7 @@ static const struct osmo_fsm_state lchan_fsm_states[] = {
 		.out_state_mask = 0
 			| S(LCHAN_ST_UNUSED)
 			| S(LCHAN_ST_WAIT_AFTER_ERROR)
+			| S(LCHAN_ST_WAIT_RF_RELEASE_ACK)
 			,
 	},
 };
