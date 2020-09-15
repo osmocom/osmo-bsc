osmocom/osmo-bsc

commit 27a887f666adb67450470d7240e25fce1d178c76

gsm 04.08: encode the LTE neighbors measurement bandwindth in Channel Release
Encoding missing measurement bandwidth of LTE neighbors in Channel Release
(Cell selection indicator after release of all TCH and SDCCH value part).
The measurement bandwidth was encoded in the neighbors description transmitted in
SI2quater while missing in the Channel Release which would overwrite the
SI2quater measurement bandwidth.

Change-Id: I4847d840ba9d5ac56bd00e4f405dc47792008c0d

src/osmo-bsc/gsm_04_08_rr.c
@@ -270,8 +270,14 @@ static int generate_cell_sel_ind_after_rel(uint8_t *out, unsigned int out_len, c
 		} else {
 			bitvec_set_bit(&bv, 1);
 			bitvec_set_uint(&bv, e->arfcn[i], 16);
-			/* No "Measurement Bandwidth" */
-			bitvec_set_bit(&bv, 0);
+
+			/* Measurement Bandwidth: 9.1.54 */
+			if (OSMO_EARFCN_MEAS_INVALID == e->meas_bw[i])
+				bitvec_set_bit(&bv, 0);
+			else {
+				bitvec_set_bit(&bv, 1);
+				bitvec_set_uint(&bv, e->meas_bw[i], 3);
+			}
 			/* No "Not Allowed Cells" */
 			bitvec_set_bit(&bv, 0);
 			/* No "TARGET_PCID" */

commit 380831e678d0cf6721bcc8389cc760a0370f7b5a

gsm 04.08: correct calculate the Cell Selection Indicator after release of all TCH and SDCCH
When the measurement bandwidth was added the calculation of the maximum
length wasn't increased.

Fixes: 27a887f666ad ("gsm 04.08: encode the LTE neighbors measurement bandwindth in Channel Release")
Change-Id: Ic8132fd988140c34b8e0fd8349f4518fcbaecc31

src/osmo-bsc/gsm_04_08_rr.c
@@ -239,8 +239,8 @@ static void mr_config_for_ms(struct gsm_lchan *lchan, struct msgb *msg)
 			lchan->mr_ms_lv + 1);
 }
 
-
-#define CELL_SEL_IND_AFTER_REL_MAX_BITS (3+MAX_EARFCN_LIST*20+1)
+#define CELL_SEL_IND_AFTER_REL_EARCFN_ENTRY (1+16+4+1+1)
+#define CELL_SEL_IND_AFTER_REL_MAX_BITS (3+MAX_EARFCN_LIST*CELL_SEL_IND_AFTER_REL_EARCFN_ENTRY+1)
 #define CELL_SEL_IND_AFTER_REL_MAX_BYTES OSMO_BYTES_FOR_BITS(CELL_SEL_IND_AFTER_REL_MAX_BITS)
 
 /* Generate a CSN.1 encoded "Cell Selection Indicator after release of all TCH and SDCCH"
@@ -263,8 +263,8 @@ static int generate_cell_sel_ind_after_rel(uint8_t *out, unsigned int out_len, c
 		if (e->arfcn[i] == OSMO_EARFCN_INVALID)
 			continue;
 
-		/* tailroom must fit one more EARFCN (20 bits), plus the final list term bit. */
-		if (bitvec_tailroom_bits(&bv) < 21) {
+		/* tailroom must fit one more EARFCN plus the final list term bit. */
+		if (bitvec_tailroom_bits(&bv) < CELL_SEL_IND_AFTER_REL_EARCFN_ENTRY + 1) {
 			LOGP(DRR, LOGL_NOTICE, "%s: Not enough room to store EARFCN %u in the "
 				"Cell Selection Indicator IE\n", gsm_bts_name(bts), e->arfcn[i]);
 		} else {