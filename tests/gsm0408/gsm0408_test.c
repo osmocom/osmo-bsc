552b4f3c0cf790d49fd73dda85f12e2888b8bf8a
Neels Hofmeyr <neels@hofmeyr.de>
2021-04-30 16:32:12 +0200

    fix test_gsm48_multirate_config: dump the complete AMR lv buffer
    It's acceptable to verify an outcome by printing to an expected output.
    It's unacceptable to commit those expected outputs without first
    verifying that they are in fact correct!
    
    In this case, the output has obviously not been even read, since the
    length byte clearly indicates that one byte is missing from each buffer
    dump.
    
    I have now verified by hand against 3GPP TS 44.018 that each one of the
    generated octets are indeed correct.
    
    Change-Id: I92fcc7afe018a4a8dc91f0f2167e3a7835f623c9

--- tests/gsm0408/gsm0408_test.c
+++ tests/gsm0408/gsm0408_test.c
@@ -810,7 +810,7 @@ static void test_gsm48_multirate_config()
 	rc = gsm48_multirate_config(lv, gsm48_ie, mr.ms_mode, 4);
 	OSMO_ASSERT(rc == 0);
 	printf("gsm48_multirate_config(): rc=%i, lv=%s\n", rc,
-	       osmo_hexdump_nospc(lv, lv[0]));
+	       osmo_hexdump_nospc(lv, 1 + lv[0]));
 
 	/* Test #2: 4 active set members, but wrong mode order: */
 	mr.ms_mode[3].mode = 2;
@@ -832,7 +832,7 @@ static void test_gsm48_multirate_config()
 	rc = gsm48_multirate_config(lv, gsm48_ie, mr.ms_mode, 3);
 	OSMO_ASSERT(rc == 0);
 	printf("gsm48_multirate_config(): rc=%i, lv=%s\n", rc,
-	       osmo_hexdump_nospc(lv, lv[0]));
+	       osmo_hexdump_nospc(lv, 1 + lv[0]));
 
 	/* Test #4: 3 active set members, but wrong mode order: */
 	mr.ms_mode[0].mode = 2;
@@ -852,7 +852,7 @@ static void test_gsm48_multirate_config()
 	rc = gsm48_multirate_config(lv, gsm48_ie, mr.ms_mode, 2);
 	OSMO_ASSERT(rc == 0);
 	printf("gsm48_multirate_config(): rc=%i, lv=%s\n", rc,
-	       osmo_hexdump_nospc(lv, lv[0]));
+	       osmo_hexdump_nospc(lv, 1 + lv[0]));
 
 	/* Test #6: 2 active set members, but wrong mode order: */
 	mr.ms_mode[1].mode = 2;
@@ -870,7 +870,7 @@ static void test_gsm48_multirate_config()
 	rc = gsm48_multirate_config(lv, gsm48_ie, mr.ms_mode, 1);
 	OSMO_ASSERT(rc == 0);
 	printf("gsm48_multirate_config(): rc=%i, lv=%s\n", rc,
-	       osmo_hexdump_nospc(lv, lv[0]));
+	       osmo_hexdump_nospc(lv, 1 + lv[0]));
 
 	/* Test #8: 0 active set members: */
 	mr.ms_mode[0].mode = 0;
