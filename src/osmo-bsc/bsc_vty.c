osmocom/osmo-bsc

commit defb5b12008006ce1c5fcca3baf23e068a23a498

drop unused gsm_bts_trx->description
Change-Id: I3c0778322b8c630b0eb9d9cd3ac3cc71386c9c12

src/osmo-bsc/bsc_vty.c
@@ -726,9 +726,6 @@ static void config_write_trx_single(struct vty *vty, struct gsm_bts_trx *trx)
 	int i;
 
 	vty_out(vty, "  trx %u%s", trx->nr, VTY_NEWLINE);
-	if (trx->description)
-		vty_out(vty, "   description %s%s", trx->description,
-			VTY_NEWLINE);
 	vty_out(vty, "   rf_locked %u%s",
 		trx->mo.force_rf_lock ? 1 : 0,
 		VTY_NEWLINE);
@@ -983,8 +980,6 @@ static void config_write_bts_single(struct vty *vty, struct gsm_bts *bts)
 
 	vty_out(vty, " bts %u%s", bts->nr, VTY_NEWLINE);
 	vty_out(vty, "  type %s%s", btstype2str(bts->type), VTY_NEWLINE);
-	if (bts->description)
-		vty_out(vty, "  description %s%s", bts->description, VTY_NEWLINE);
 	vty_out(vty, "  band %s%s", gsm_band_name(bts->band), VTY_NEWLINE);
 	vty_out(vty, "  cell_identity %u%s", bts->cell_identity, VTY_NEWLINE);
 	vty_out(vty, "  location_area_code %u%s", bts->location_area_code,
@@ -1335,8 +1330,6 @@ static void trx_dump_vty(struct vty *vty, struct gsm_bts_trx *trx, bool print_rs
 
 	vty_out(vty, "TRX %u of BTS %u is on ARFCN %u%s",
 		trx->nr, trx->bts->nr, trx->arfcn, VTY_NEWLINE);
-	vty_out(vty, "Description: %s%s",
-		trx->description ? trx->description : "(null)", VTY_NEWLINE);
 	vty_out(vty, "  RF Nominal Power: %d dBm, reduced by %u dB, "
 		"resulting BS power: %d dBm%s",
 		trx->nominal_power, trx->max_power_red,
@@ -5322,7 +5315,6 @@ DEFUN_ATTR(cfg_trx,
 		return CMD_WARNING;
 
 	vty->index = trx;
-	vty->index_sub = &trx->description;
 	vty->node = TRX_NODE;
 
 	return CMD_SUCCESS;

include/osmocom/bsc/bts_trx.h
@@ -31,8 +31,6 @@ struct gsm_bts_trx {
 	struct gsm_bts *bts;
 	/* number of this TRX in the BTS */
 	uint8_t nr;
-	/* human readable name / description */
-	char *description;
 	/* how do we talk RSL with this TRX? */
 	struct gsm_e1_subslot rsl_e1_link;
 	uint8_t rsl_tei;

src/osmo-bsc/osmo_bsc_main.c
@@ -607,7 +607,6 @@ static int bsc_vty_go_parent(struct vty *vty)
 			/* set vty->index correctly ! */
 			struct gsm_bts_trx_ts *ts = vty->index;
 			vty->index = ts->trx;
-			vty->index_sub = &ts->trx->description;
 		}
 		break;
 	case OML_NODE:

commit 875f5131834b72dadbf9e21600fae6557b604a6f

Fix bts->description field not printed in config write
A recent commit removed the references to the per-TRX description, but
wrongly removed the per-BTS in this place too.

Fixes: defb5b12008006ce1c5fcca3baf23e068a23a498
Change-Id: Iacaa2a25633a8706bfde9e0c196fee3e5bff1639

src/osmo-bsc/bsc_vty.c
@@ -981,6 +981,8 @@ static void config_write_bts_single(struct vty *vty, struct gsm_bts *bts)
 
 	vty_out(vty, " bts %u%s", bts->nr, VTY_NEWLINE);
 	vty_out(vty, "  type %s%s", btstype2str(bts->type), VTY_NEWLINE);
+	if (bts->description)
+		vty_out(vty, "  description %s%s", bts->description, VTY_NEWLINE);
 	vty_out(vty, "  band %s%s", gsm_band_name(bts->band), VTY_NEWLINE);
 	vty_out(vty, "  cell_identity %u%s", bts->cell_identity, VTY_NEWLINE);
 	vty_out(vty, "  location_area_code %u%s", bts->location_area_code,