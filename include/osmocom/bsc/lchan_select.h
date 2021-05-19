40fba410e110af85f93ceffc8f528d544934b811
Neels Hofmeyr <neels@hofmeyr.de>
2021-05-19 02:32:33 +0200

    add chan_mode_to_chan_type()
    Move the conversion from chan_mode to lchan type out of the
    lchan_select_by_chan_mode() function, so that the conversion can be used
    by other code paths, coming up in VAMOS patches.
    
    Related: SYS#5315 OS#4940
    Change-Id: I296651ebadba81f8b3db0d9bb5b5377877a43677

--- include/osmocom/bsc/lchan_select.h
+++ include/osmocom/bsc/lchan_select.h
@@ -2,6 +2,7 @@
 #pragma once
 
 struct gsm_lchan *lchan_select_by_type(struct gsm_bts *bts, enum gsm_chan_t type);
+enum gsm_chan_t chan_mode_to_chan_type(enum gsm48_chan_mode chan_mode, enum channel_rate chan_rate);
 struct gsm_lchan *lchan_select_by_chan_mode(struct gsm_bts *bts,
 					    enum gsm48_chan_mode chan_mode, enum channel_rate chan_rate);
 struct gsm_lchan *lchan_avail_by_type(struct gsm_bts *bts, enum gsm_chan_t type, bool log);
