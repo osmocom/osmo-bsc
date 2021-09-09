osmocom/osmo-bsc

commit 997a257f8dabe5dd940a1271e56e676a871896d7

power_control: constrain BS power reduction on BCCH carrier
BS Power Control is not allowed on the BCCH/CCCH carrier, unless
the BTS is operating in the BCCH carrier power reduction mode.

Allow constrained BS power reduction (up to 6 dB) on active logical
channels iff BCCH carrier power reduction mode is enabled.

Take into account that the maximum power difference between a timeslot
used for BCCH/CCCH and the timeslot preceding it shall not exceed 3 dB.

Change-Id: I2cc6a86731984f586ef35b43a8d3de631f7d8a2f
Related: SYS#4919, SYS#4918

src/osmo-bsc/lchan_fsm.c
@@ -675,6 +675,13 @@ static void lchan_fsm_wait_ts_ready_onenter(struct osmo_fsm_inst *fi, uint32_t p
 			lchan->bs_power_db = bts->bs_power_ctrl.bs_power_val_db;
 	}
 
+	/* BS Power Control is generally not allowed on the BCCH/CCCH carrier.
+	 * However, we allow it in the BCCH carrier power reduction mode of operation. */
+	if (lchan->ts->trx == bts->c0) {
+		lchan->bs_power_db = OSMO_MIN(lchan->ts->c0_max_power_red_db,
+					      lchan->bs_power_db);
+	}
+
 	if (lchan_activate_set_ch_mode_rate_and_mr_config(lchan))
 		return;
 

include/osmocom/bsc/gsm_data.h
@@ -841,6 +841,9 @@ struct gsm_bts_trx_ts {
 		} rbs2000;
 	};
 
+	/* Maximum BCCH carrier power reduction */
+	uint8_t c0_max_power_red_db;
+
 	/* Maximum number of lchans that could become usable, for example by switching a dynamic timeslot's type or by
 	 * enabling VAMOS secondary lchans. This does include the maximum count of possible VAMOS secondary lchans. */
 	uint8_t max_lchans_possible;

src/osmo-bsc/bts.c
@@ -749,6 +749,8 @@ int gsm_bts_set_system_infos(struct gsm_bts *bts)
 
 int gsm_bts_set_c0_power_red(struct gsm_bts *bts, const uint8_t red)
 {
+	struct gsm_bts_trx *c0 = bts->c0;
+	unsigned int tn;
 	int rc;
 
 	if (!osmo_bts_has_feature(&bts->features, BTS_FEAT_BCCH_POWER_RED))
@@ -759,6 +761,35 @@ int gsm_bts_set_c0_power_red(struct gsm_bts *bts, const uint8_t red)
 	rc = bts->model->power_ctrl_set_c0_power_red(bts, red);
 	if (rc != 0)
 		return rc;
+
+	/* Timeslot 0 is always transmitting BCCH/CCCH */
+	c0->ts[0].c0_max_power_red_db = 0;
+
+	for (tn = 1; tn < ARRAY_SIZE(c0->ts); tn++) {
+		struct gsm_bts_trx_ts *ts = &c0->ts[tn];
+		struct gsm_bts_trx_ts *prev = ts - 1;
+
+		switch (ts->pchan_is) {
+		/* Not allowed on CCCH/BCCH */
+		case GSM_PCHAN_CCCH:
+			/* Preceeding timeslot shall not exceed 2 dB */
+			if (prev->c0_max_power_red_db > 0)
+				prev->c0_max_power_red_db = 2;
+			/* fall-through */
+		/* Not recommended on SDCCH/8 */
+		case GSM_PCHAN_SDCCH8_SACCH8C:
+		case GSM_PCHAN_SDCCH8_SACCH8C_CBCH:
+			ts->c0_max_power_red_db = 0;
+			break;
+		default:
+			ts->c0_max_power_red_db = red;
+			break;
+		}
+	}
+
+	/* Timeslot 7 is always preceding BCCH/CCCH */
+	if (c0->ts[7].c0_max_power_red_db > 0)
+		c0->ts[7].c0_max_power_red_db = 2;
 
 	bts->c0_max_power_red_db = red;
 

commit df93b188ec73191df57dae5f6150c2b20689060a

lchan_fsm: Avoid inheriting bs_power from old lchan
So far we were inherting the bs_power in used when activating an lchan
for an MS for which we already had previous lchan. For instance, when an
MS is first assigned an SDCCH, and later on, it is assigned a TCH to
place a voice call.
Doing so is, however, not correct because current and old lchans may be
placed on different TS or even on different TRX, which means they will
have different BS Power restrictions. In the scenario described above,
for instance, an SDCCH could have been assigned on C0 and hence have
a bs_power of 0, and later on, whenever the TCH lchan as created, it
would have inherited the C0 bs_power despite it may have been possible
to use a different (lower) max bs power.
Furthermore, the lchan->bs_power_db basically stores the *maximum* bs
power reduction. Hence it makes not sense at all to copy over the value,
since it is anyway not updated from MS measurement reports so far
anyway.

Fixes: 997a257f8dabe5dd940a1271e56e676a871896d7
Related: SYS#4919
Change-Id: I4a7736aa9a1395e0cc118b98b69896bd0f1e94e6

src/osmo-bsc/lchan_fsm.c
@@ -681,7 +681,7 @@ static void lchan_fsm_wait_ts_ready_onenter(struct osmo_fsm_inst *fi, uint32_t p
 	struct osmo_mgcpc_ep_ci *use_mgwep_ci;
 	struct gsm_lchan *old_lchan = lchan->activate.info.re_use_mgw_endpoint_from_lchan;
 	struct lchan_activate_info *info = &lchan->activate.info;
-	int ms_power_dbm;
+	int ms_power_dbm = bts->ms_max_power;
 
 	if (lchan->release.requested) {
 		lchan_fail("Release requested while activating");
@@ -691,23 +691,17 @@ static void lchan_fsm_wait_ts_ready_onenter(struct osmo_fsm_inst *fi, uint32_t p
 	lchan->conn = info->for_conn;
 
 	/* If there is a previous lchan, and the new lchan is on the same cell as previous one,
-	 * take over power values. Otherwise, use max power. */
+	 * take over MS power values. Otherwise, use configured MS max power. */
 	if (old_lchan && old_lchan->ts->trx->bts == bts) {
-		ms_power_dbm = ms_pwr_dbm(bts->band, old_lchan->ms_power);
-		lchan_update_ms_power_ctrl_level(lchan, ms_power_dbm >= 0 ? ms_power_dbm : bts->ms_max_power);
-		lchan->bs_power_db = old_lchan->bs_power_db;
-	} else {
-		lchan_update_ms_power_ctrl_level(lchan, bts->ms_max_power);
-		/* Upon last entering the UNUSED state, from lchan_reset():
-		 * - bs_power_db is still zero, 0dB reduction, output power = Pn. */
-
-		/* Default BS Power reduction value (in dB) */
-		if (bts->bs_power_ctrl.mode == GSM_PWR_CTRL_MODE_DYN_BTS)
-			lchan->bs_power_db = bts->bs_power_ctrl.bs_power_max_db;
-		else
-			lchan->bs_power_db = bts->bs_power_ctrl.bs_power_val_db;
+		if ((ms_power_dbm = ms_pwr_dbm(bts->band, old_lchan->ms_power)) == 0)
+			ms_power_dbm = bts->ms_max_power;
 	}
+	lchan_update_ms_power_ctrl_level(lchan, ms_power_dbm);
 
+	/* Default BS Power reduction value (in dB) */
+	lchan->bs_power_db = (bts->bs_power_ctrl.mode == GSM_PWR_CTRL_MODE_DYN_BTS) ?
+				bts->bs_power_ctrl.bs_power_max_db :
+				bts->bs_power_ctrl.bs_power_val_db;
 	/* BS Power Control is generally not allowed on the BCCH/CCCH carrier.
 	 * However, we allow it in the BCCH carrier power reduction mode of operation. */
 	if (lchan->ts->trx == bts->c0) {