osmocom/osmo-bsc

commit 4821c9f4dff3cb9d06ee8392de18fd7c63399bf0

paging: Recalculate work timer if waiting for retrans
If the queue is only holding requests in retransmition state, when we
add a new one, we have to re-calculate the work timer to a lower value
instead of letting it wait for the first retransmit to be ready.

Change-Id: Ibd4f8921c92f7481f0b9943041c141640ab812c8

src/osmo-bsc/paging.c
@@ -432,7 +432,28 @@ static int _paging_request(const struct bsc_paging_params *params, struct gsm_bt
 
 	t3113_timeout_s = calculate_timer_3113(req, reqs_before_same_pgroup);
 	osmo_timer_schedule(&req->T3113, t3113_timeout_s, 0);
-	paging_schedule_if_needed(bts_entry);
+
+	/* Trigger scheduler if needed: */
+	if (!osmo_timer_pending(&bts_entry->work_timer)) {
+		paging_handle_pending_requests(bts_entry);
+	} else if (last_initial_req == NULL) {
+		/* Worker timer is armed -> there was already one req before
+		 * last_initial_req is NULL -> There were no initial requests in
+		 *       the list, aka the timer is waiting for retransmition,
+		 *       which is a longer period.
+		 * Let's recaculate the time to adapt it to initial_period: */
+		struct timespec now, elapsed, tdiff;
+		osmo_clock_gettime(CLOCK_MONOTONIC, &now);
+		/* This is what used to be the first req (retrans state) in the queue: */
+		req = llist_entry(req->entry.next, struct gsm_paging_request, entry);
+		timespecsub(&now, &req->last_attempt_ts, &elapsed);
+		if (timespeccmp(&elapsed, &initial_period, <)) {
+			timespecsub(&initial_period, &elapsed, &tdiff);
+		} else {
+			tdiff = (struct timespec){.tv_sec = 0, .tv_nsec = 0 };
+		}
+		osmo_timer_schedule(&bts_entry->work_timer, tdiff.tv_sec, tdiff.tv_nsec / 1000);
+	} /* else: worker is already ongoing submitting initial requests, nothing do be done */
 
 	return 0;
 }

commit e11fb6cd9dedf228eecd65aa2520ba1ddcfd9209

paging: Fix recalculate work timer if waiting for retrans
We want to recalculate the timer based on last time the work_timer was
triggered (that is, the time when the worker re-armed the pag req to
retransmit). We don't want to recalculate based on the last time the pag
ret tro retransmit was scheduled.

In loaded paging queue, there's lots of retrans (let's say 200) and it
may take more than 500ms to actually retransmit them. That means in most
cases we could end up in a situation where only pag req to retrans where
in the queue, hitting this recalculate path. Since the 500ms were for
sure elapsed, that would most probably schedule the work_timer at {0,0}
for each new paging request that arrived. As a result, the worker would
be scheduled lots of times per second (once for each new req arriving)
and only submitting 1 pag req (the new one) plus potentially 1 or
serveral pag req to retransmit.

In summary, there was not throthling applied in the scenario where only
pag req to retransmit where in the queue and new pag reqs kept arriving.
This incurrs into augmented paging throughput and also augmented
frequency of polls().

Related: OS#5922
Fixes: 4821c9f4dff3cb9d06ee8392de18fd7c63399bf0
Change-Id: I7ce6f436286b50dc31331d218ff256cf7be3f619

include/osmocom/bsc/paging.h
@@ -106,6 +106,9 @@ struct gsm_bts_paging_state {
 	struct osmo_timer_list work_timer;
 	struct osmo_timer_list credit_timer;
 
+	/* Last time paging worker was triggered */
+	struct timespec last_sched_ts;
+
 	/* free chans needed */
 	int free_chans_need;
 

src/osmo-bsc/paging.c
@@ -225,6 +225,7 @@ static void paging_handle_pending_requests(struct gsm_bts_paging_state *paging_b
 		goto sched_next_iter;
 
 	osmo_clock_gettime(CLOCK_MONOTONIC, &now);
+	paging_bts->last_sched_ts = now;
 
 	/* do while loop: Try send at most first MAX_PAGE_REQ_PER_ITER paging
 	 * requests (or before if there are no more available slots). Since
@@ -458,11 +459,8 @@ static int _paging_request(const struct bsc_paging_params *params, struct gsm_bt
 		 *       which is a longer period.
 		 * Let's recaculate the time to adapt it to initial_period: */
 		struct timespec now, elapsed, tdiff;
-		struct gsm_paging_request *first_retrans_req;
 		osmo_clock_gettime(CLOCK_MONOTONIC, &now);
-		/* This is what used to be the first req (retrans state) in the queue: */
-		first_retrans_req = llist_entry(req->entry.next, struct gsm_paging_request, entry);
-		timespecsub(&now, &first_retrans_req->last_attempt_ts, &elapsed);
+		timespecsub(&now, &bts_entry->last_sched_ts, &elapsed);
 		if (timespeccmp(&elapsed, &initial_period, <)) {
 			timespecsub(&initial_period, &elapsed, &tdiff);
 		} else {