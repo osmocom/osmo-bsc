4bcfb7e0342ac0ab956b559347548d6afda572b3
Oliver Smith <osmith@sysmocom.de>
2022-11-25 10:56:35 +0100

    fsms: use configurable timers instead of T23042
    T23042 was a placeholder for timers to be filled in later. Replace it
    with timers that can be configured via VTY.
    
    Previous timeout for the states was 5s, from using the default of 5 for
    undefined timers.
    
    * ASSIGNMENT_ST_WAIT_MGW_ENDPOINT_TO_MSC and
      HO_ST_WAIT_MGW_ENDPOINT_TO_MSC:
      * Runs gscon_connect_mgw_to_msc() on enter, which waits for one MGCP
        CRCX or MDCX response (or changes state immediately)
      * Use existing X9 ("Timeout for availability of MGW endpoint"), 5s,
        which is already being used by lchan_rtp_fsm for a similar purpose
    
    * HO_ST_WAIT_RR_HO_DETECT:
      * Handover initiation as described in 3GPP TS 04.08 § 3.4.4.1:
        "The network initiates the handover procedure by sending a HANDOVER
         COMMAND message to the mobile station on the main DCCH. It then
         starts timer T3103."
      * Use existing but unused timer T3103 ("Handover"), 5s
    
    * HO_ST_WAIT_RR_HO_COMPLETE:
      * Handover completion as described in 3GPP TS 04.08 § 3.4.4.3:
        "When receiving the HANDOVER COMPLETE message, the network stops
         timer T3103 and releases the old channels."
      * Continue using T3103 with keep_timer = true
    
    Closes: OS#5787
    Change-Id: Id0d4d0788f609f3272fc81c80a754383dde25c16

--- src/osmo-bsc/handover_fsm.c
+++ src/osmo-bsc/handover_fsm.c
@@ -194,9 +194,9 @@ struct gsm_subscriber_connection *ho_fi_conn(struct osmo_fsm_inst *fi)
 
 static const struct osmo_tdef_state_timeout ho_fsm_timeouts[32] = {
 	[HO_ST_WAIT_LCHAN_ACTIVE] = { /* Guarded by X5 + X6 in lchan_fsm_timeouts */ },
-	[HO_ST_WAIT_MGW_ENDPOINT_TO_MSC] = { .T = 23042 },
-	[HO_ST_WAIT_RR_HO_DETECT] = { .T = 23042 },
-	[HO_ST_WAIT_RR_HO_COMPLETE] = { .T = 23042 },
+	[HO_ST_WAIT_MGW_ENDPOINT_TO_MSC] = { .T = -9 },
+	[HO_ST_WAIT_RR_HO_DETECT] = { .T = 3103 },
+	[HO_ST_WAIT_RR_HO_COMPLETE] = { .keep_timer = true }, /* Keep T3103 */
 	[HO_ST_WAIT_LCHAN_ESTABLISHED] = { /* Guarded by T3101 in lchan_fsm_timeouts */ },
 	[HO_OUT_ST_WAIT_HO_COMMAND] = { .T = 7 },
 	[HO_OUT_ST_WAIT_CLEAR] = { .T = 8 },
