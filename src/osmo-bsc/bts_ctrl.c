1dd9afe582929070dcd27258f31742c1d3a52858
Matan Perelman <matan1008@gmail.com>
2023-05-17 19:15:09 +0300

    ctrl: Add bsic
    Change-Id: I5c941770924e5373733c3a3939671fab518f7a58

--- src/osmo-bsc/bts_ctrl.c
+++ src/osmo-bsc/bts_ctrl.c
@@ -248,6 +248,7 @@ err:
 /* BTS related commands below */
 CTRL_CMD_DEFINE_RANGE(bts_lac, "location-area-code", struct gsm_bts, location_area_code, 0, 65535);
 CTRL_CMD_DEFINE_RANGE(bts_ci, "cell-identity", struct gsm_bts, cell_identity, 0, 65535);
+CTRL_CMD_DEFINE_RANGE(bts_bsic, "bsic", struct gsm_bts, bsic, 0, 63);
 
 static int set_bts_apply_config(struct ctrl_cmd *cmd, void *data)
 {
@@ -1045,6 +1046,7 @@ int bsc_bts_ctrl_cmds_install(void)
 	rc |= ctrl_cmd_install(CTRL_NODE_BTS, &cmd_bts_loc);
 	rc |= ctrl_cmd_install(CTRL_NODE_BTS, &cmd_bts_lac);
 	rc |= ctrl_cmd_install(CTRL_NODE_BTS, &cmd_bts_ci);
+	rc |= ctrl_cmd_install(CTRL_NODE_BTS, &cmd_bts_bsic);
 	rc |= ctrl_cmd_install(CTRL_NODE_BTS, &cmd_bts_apply_config);
 	rc |= ctrl_cmd_install(CTRL_NODE_BTS, &cmd_bts_si);
 	rc |= ctrl_cmd_install(CTRL_NODE_BTS, &cmd_bts_power_ctrl_defs);
