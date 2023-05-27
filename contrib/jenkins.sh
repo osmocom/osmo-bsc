osmocom/osmo-bsc

commit d33a66b779ea021a7bfeb83a436e9421e1bee6e0

contrib/jenkins: create workspace.tar.xz on error
In order to figure out why we sometimes get a coredump in the jenkins
master jobs, add a quick hack to get all relevant binaries on libraries
on error.

Related: OS#5665
Change-Id: I1439c06316edbe11f162c14774c2d507152b97a7

contrib/jenkins.sh
@@ -4,7 +4,19 @@
 # environment variables:
 # * WITH_MANUALS: build manual PDFs if set to "1"
 # * PUBLISH: upload manuals after building if set to "1" (ignored without WITH_MANUALS = "1")
+# * IS_MASTER_BUILD: set to 1 when running from master-builds (not gerrit-verifications)
 #
+
+exit_tar_workspace() {
+	cat-testlogs.sh
+
+	if [ "$IS_MASTER_BUILD" = "1" ]; then
+		tar -cJf "/tmp/workspace.tar.xz" "$base"
+		mv /tmp/workspace.tar.xz "$base"
+	fi
+
+	exit 1
+}
 
 if ! [ -x "$(command -v osmo-build-dep.sh)" ]; then
 	echo "Error: We need to have scripts/osmo-deps.sh from http://git.osmocom.org/osmo-ci/ in PATH !"
@@ -68,11 +80,11 @@ autoreconf --install --force
 ./configure --enable-sanitize --enable-external-tests --enable-werror $CONFIG
 $MAKE $PARALLEL_MAKE
 LD_LIBRARY_PATH="$inst/lib" $MAKE check \
-  || cat-testlogs.sh
+  || exit_tar_workspace
 LD_LIBRARY_PATH="$inst/lib" \
   DISTCHECK_CONFIGURE_FLAGS="--enable-vty-tests --enable-external-tests --enable-werror $CONFIG" \
   $MAKE $PARALLEL_MAKE distcheck \
-  || cat-testlogs.sh
+  || exit_tar_workspace
 
 if [ "$WITH_MANUALS" = "1" ] && [ "$PUBLISH" = "1" ]; then
 	make -C "$base/doc/manuals" publish

commit ff3540cb2c9aa1a06d10b7bf9f85be4620dbde66

fixup: contrib/jenkins: create workspace.tar.xz on error
cat-testlogs.sh does "exit 1", so no workspace.tar.xz is created.
Call this script after archiving the workspace.

Change-Id: Ibcb842f32418e66a186d6b21bb5861cf4a0b7c4a
Fixes: d33a66b779 "contrib/jenkins: create workspace.tar.xz on error"
Related: OS#5665

contrib/jenkins.sh
@@ -8,14 +8,12 @@
 #
 
 exit_tar_workspace() {
-	cat-testlogs.sh
-
 	if [ "$IS_MASTER_BUILD" = "1" ]; then
 		tar -cJf "/tmp/workspace.tar.xz" "$base"
 		mv /tmp/workspace.tar.xz "$base"
 	fi
 
-	exit 1
+	cat-testlogs.sh
 }
 
 if ! [ -x "$(command -v osmo-build-dep.sh)" ]; then