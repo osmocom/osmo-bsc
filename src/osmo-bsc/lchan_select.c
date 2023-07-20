osmocom/osmo-bsc

commit d5eb0f1b57f37778aac28f6c36b2a00744420ffb

lchan_select: chan_mode_to_chan_type: support CSD
Related: OS#4393
Change-Id: I6b02d7edbf268a7f07134b54a7289529b2485122

src/osmo-bsc/lchan_select.c
@@ -184,6 +184,10 @@ enum gsm_chan_t chan_mode_to_chan_type(enum gsm48_chan_mode chan_mode, enum chan
 		/* fall through */
 	case GSM48_CMODE_SPEECH_V1:
 	case GSM48_CMODE_SPEECH_AMR:
+	case GSM48_CMODE_DATA_14k5:
+	case GSM48_CMODE_DATA_12k0:
+	case GSM48_CMODE_DATA_6k0:
+	case GSM48_CMODE_DATA_3k6:
 		switch (chan_rate) {
 		case CH_RATE_HALF:
 			return GSM_LCHAN_TCH_H;

commit 121461669e52dd395916fab7ceaf0b26115e5d95

lchan_select: fix lchan selection for GSM48_CMODE_DATA_{14k5,12k0}
Change-Id: I271ebf7e64e3d0861586e9bc63bec113cb5fb410
Fixes: d5eb0f1b5 "lchan_select: chan_mode_to_chan_type: support CSD"
Related: OS#4393

src/osmo-bsc/lchan_select.c
@@ -178,14 +178,14 @@ enum gsm_chan_t chan_mode_to_chan_type(enum gsm48_chan_mode chan_mode, enum chan
 			return GSM_LCHAN_NONE;
 		}
 	case GSM48_CMODE_SPEECH_EFR:
-		/* EFR works over FR channels only */
+	case GSM48_CMODE_DATA_14k5:
+	case GSM48_CMODE_DATA_12k0:
+		/* these rates work over full-rate channels only */
 		if (chan_rate != CH_RATE_FULL)
 			return GSM_LCHAN_NONE;
 		/* fall through */
 	case GSM48_CMODE_SPEECH_V1:
 	case GSM48_CMODE_SPEECH_AMR:
-	case GSM48_CMODE_DATA_14k5:
-	case GSM48_CMODE_DATA_12k0:
 	case GSM48_CMODE_DATA_6k0:
 	case GSM48_CMODE_DATA_3k6:
 		switch (chan_rate) {