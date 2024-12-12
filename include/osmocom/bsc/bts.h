osmocom/osmo-bsc

commit 6fe0e28a23a3aa3e707d147569add7f68d5dc4a1

Introduce typedef for bts->nr gsm_bts_nr_t
The size of bts->nr is implicitly or explicitly used or taken into
account in several places in the code.
Add a new type to make it easier to define its size and range in one
place.

This also helps in identifying fields contains an BSC-implementation
"bts_nr" vs theip.access Abis OML "bts_nr" sent over the wire, which is
always a uint8_t according to spec.

Related: SYS#7062
Change-Id: Ic0db2873fa9795024aba4399da495a0d8f7504b0

include/osmocom/bsc/bts.h
@@ -329,7 +329,7 @@ struct gsm_gprs_cell {
 /* See (struct gsm_bts *)->depends_on */
 struct bts_depends_on_entry {
 	struct llist_head list;
-	uint8_t bts_nr; /* See (struct gsm_bts *)->nr */
+	gsm_bts_nr_t bts_nr; /* See (struct gsm_bts *)->nr */
 };
 
 /* One BTS */
@@ -341,7 +341,7 @@ struct gsm_bts {
 	struct llist_head loc_list;
 
 	/* number of this BTS in network */
-	uint8_t nr;
+	gsm_bts_nr_t nr;
 	/* human readable name / description */
 	char *description;
 	/* Cell Identity */
@@ -836,8 +836,8 @@ static inline bool gsm_bts_features_negotiated(struct gsm_bts *bts)
 }
 
 /* dependency handling */
-int bts_depend_mark(struct gsm_bts *bts, int dep);
-void bts_depend_clear(struct gsm_bts *bts, int dep);
+int bts_depend_mark(struct gsm_bts *bts, gsm_bts_nr_t dep);
+void bts_depend_clear(struct gsm_bts *bts, gsm_bts_nr_t dep);
 bool bts_depend_check(struct gsm_bts *bts);
 bool bts_depend_is_depedency(struct gsm_bts *base, struct gsm_bts *other);
 

src/osmo-bsc/bts.c
@@ -870,7 +870,7 @@ void gsm_bts_mo_reset(struct gsm_bts *bts)
 	}
 }
 
-int bts_depend_mark(struct gsm_bts *bts, int dep)
+int bts_depend_mark(struct gsm_bts *bts, gsm_bts_nr_t dep)
 {
 	struct bts_depends_on_entry *entry;
 	entry = talloc_zero(bts, struct bts_depends_on_entry);
@@ -883,7 +883,7 @@ int bts_depend_mark(struct gsm_bts *bts, int dep)
 	return 0;
 }
 
-static struct bts_depends_on_entry *bts_depend_find_entry(const struct gsm_bts *bts, int dep)
+static struct bts_depends_on_entry *bts_depend_find_entry(const struct gsm_bts *bts, gsm_bts_nr_t dep)
 {
 	struct bts_depends_on_entry *entry;
 	llist_for_each_entry(entry, &bts->trx_list, list) {
@@ -893,7 +893,7 @@ static struct bts_depends_on_entry *bts_depend_find_entry(const struct gsm_bts *
 	return NULL;
 }
 
-void bts_depend_clear(struct gsm_bts *bts, int dep)
+void bts_depend_clear(struct gsm_bts *bts, gsm_bts_nr_t dep)
 {
 	struct bts_depends_on_entry *entry;
 	entry = bts_depend_find_entry(bts, dep);

include/osmocom/bsc/gsm_data.h
@@ -21,6 +21,7 @@
 #include <osmocom/core/time_cc.h>
 #include <osmocom/core/linuxlist.h>
 #include <osmocom/core/linuxrbtree.h>
+#include <osmocom/core/utils.h>
 
 #include <osmocom/crypt/auth.h>
 
@@ -95,6 +96,13 @@ struct smlc_config;
  *   other values are reserved. */
 #define DLCI2RSL_LINK_ID(dlci) \
 	((dlci & 0xc0) == 0xc0 ? 0x40 : 0x00) | (dlci & 0x07)
+
+typedef uint8_t gsm_bts_nr_t; /* See (struct gsm_bts *)->nr */
+/* BTS_NR_MAX = ((2 << ((sizeof(gsm_bts_nr_t) * 8) - 1)) - 1)
+ * This is needed as a constant so that the value can be stringified properly: */
+#define BTS_NR_MAX 255
+#define BTS_NR_MAX_STR OSMO_STRINGIFY_VAL(BTS_NR_MAX)
+#define BTS_NR_VTY_ARG_VAL "<0-" BTS_NR_MAX_STR ">"
 
 /* 3-bit long values */
 #define EARFCN_PRIO_INVALID 8
@@ -881,7 +889,7 @@ extern struct osmo_tdef_group bsc_tdef_group[];
 
 struct gsm_network *gsm_network_init(void *ctx);
 
-struct gsm_bts *gsm_bts_num(const struct gsm_network *net, int num);
+struct gsm_bts *gsm_bts_num(const struct gsm_network *net, gsm_bts_nr_t num);
 struct gsm_bts *gsm_bts_by_cell_id(const struct gsm_network *net,
 				   const struct gsm0808_cell_id *cell_id,
 				   int match_idx);
@@ -975,7 +983,7 @@ struct gsm_network {
 	struct rate_ctr_group *bsc_ctrs;
 	struct osmo_stat_item_group *bsc_statg;
 
-	unsigned int num_bts;
+	gsm_bts_nr_t num_bts;
 	struct llist_head bts_list;
 	struct llist_head bts_rejected;
 

src/osmo-bsc/abis_nm_vty.c
@@ -76,7 +76,7 @@ struct oml_node_state {
 
 
 DEFUN(oml_class_inst, oml_class_inst_cmd,
-	"bts <0-255> oml class " NM_OBJCLASS_VTY
+	"bts " BTS_NR_VTY_ARG_VAL " oml class " NM_OBJCLASS_VTY
 					" instance <0-255> <0-255> <0-255>",
 	"BTS related commands\n" "BTS Number\n"
 	"Manipulate the OML managed objects\n"
@@ -111,7 +111,7 @@ DEFUN(oml_class_inst, oml_class_inst_cmd,
 }
 
 DEFUN(oml_classnum_inst, oml_classnum_inst_cmd,
-	"bts <0-255> oml class <0-255> instance <0-255> <0-255> <0-255>",
+	"bts " BTS_NR_VTY_ARG_VAL " oml class <0-255> instance <0-255> <0-255> <0-255>",
 	"BTS related commands\n" "BTS Number\n"
 	"Manipulate the OML managed objects\n"
 	"Object Class\n" "Object Class\n"

src/osmo-bsc/abis_om2000_vty.c
@@ -79,7 +79,7 @@ struct oml_node_state {
 #define OM2K_VTY_HELP "Configure OM2K specific parameters\n"
 
 DEFUN(om2k_class_inst, om2k_class_inst_cmd,
-	"bts <0-255> om2000 class " OM2K_OBJCLASS_VTY
+	"bts " BTS_NR_VTY_ARG_VAL " om2000 class " OM2K_OBJCLASS_VTY
 					" <0-255> <0-255> <0-255>",
 	"BTS related commands\n" "BTS Number\n"
 	"Manipulate the OM2000 managed objects\n"
@@ -120,7 +120,7 @@ DEFUN(om2k_class_inst, om2k_class_inst_cmd,
 }
 
 DEFUN(om2k_classnum_inst, om2k_classnum_inst_cmd,
-	"bts <0-255> om2000 class <0-255> <0-255> <0-255> <0-255>",
+	"bts " BTS_NR_VTY_ARG_VAL " om2000 class <0-255> <0-255> <0-255> <0-255>",
 	"BTS related commands\n" "BTS Number\n"
 	"Manipulate the OML managed objects\n"
 	"Object Class\n" "Object Class\n"
@@ -748,7 +748,7 @@ static void vty_dump_om2k_mo(struct vty *vty, const struct om2k_mo *mo, const ch
 }
 
 DEFUN(show_om2k_mo, show_om2k_mo_cmd,
-	"show bts <0-255> om2k-mo",
+	"show bts " BTS_NR_VTY_ARG_VAL " om2k-mo",
 	SHOW_STR "Display information about a BTS\n"
 	"BTS number\n" "OM2000 Managed Object information\n")
 {

src/osmo-bsc/bsc_vty.c
@@ -232,7 +232,7 @@ DEFUN(bsc_show_net, bsc_show_net_cmd, "show network",
 
 	return CMD_SUCCESS;
 }
-DEFUN(show_bts, show_bts_cmd, "show bts [<0-255>]",
+DEFUN(show_bts, show_bts_cmd, "show bts [" BTS_NR_VTY_ARG_VAL "]",
 	SHOW_STR "Display information about a BTS\n"
 		"BTS number\n")
 {
@@ -273,7 +273,7 @@ DEFUN(show_bts_brief, show_bts_brief_cmd, "show bts brief",
 	return CMD_SUCCESS;
 }
 
-DEFUN(show_bts_fail_rep, show_bts_fail_rep_cmd, "show bts <0-255> fail-rep [reset]",
+DEFUN(show_bts_fail_rep, show_bts_fail_rep_cmd, "show bts " BTS_NR_VTY_ARG_VAL " fail-rep [reset]",
 	SHOW_STR "Display information about a BTS\n"
 		"BTS number\n" "OML failure reports\n"
 		"Clear the list of failure reports after showing them\n")
@@ -457,7 +457,7 @@ static inline void print_all_trx(struct vty *vty, const struct gsm_bts *bts)
 
 DEFUN(show_trx,
       show_trx_cmd,
-      "show trx [<0-255>] [<0-255>]",
+      "show trx [" BTS_NR_VTY_ARG_VAL "] [<0-255>]",
 	SHOW_STR "Display information about a TRX\n"
 	BTS_TRX_STR)
 {
@@ -500,7 +500,7 @@ DEFUN(show_trx,
 
 DEFUN(show_ts,
       show_ts_cmd,
-      "show timeslot [<0-255>] [<0-255>] [<0-7>]",
+      "show timeslot [" BTS_NR_VTY_ARG_VAL "] [<0-255>] [<0-7>]",
 	SHOW_STR "Display information about a TS\n"
 	BTS_TRX_TS_STR)
 {
@@ -727,7 +727,7 @@ static int lchan_summary(struct vty *vty, int argc, const char **argv,
 
 DEFUN(show_lchan,
       show_lchan_cmd,
-      "show lchan [<0-255>] [<0-255>] [<0-7>] [<0-7>]",
+      "show lchan [" BTS_NR_VTY_ARG_VAL "] [<0-255>] [<0-7>] [<0-7>]",
 	SHOW_STR "Display information about a logical channel\n"
 	BTS_TRX_TS_LCHAN_STR)
 {
@@ -736,7 +736,7 @@ DEFUN(show_lchan,
 
 DEFUN(show_lchan_summary,
       show_lchan_summary_cmd,
-      "show lchan summary [<0-255>] [<0-255>] [<0-7>] [<0-7>]",
+      "show lchan summary [" BTS_NR_VTY_ARG_VAL "] [<0-255>] [<0-7>] [<0-7>]",
 	SHOW_STR "Display information about a logical channel\n"
         "Short summary (used lchans)\n"
 	BTS_TRX_TS_LCHAN_STR)
@@ -746,7 +746,7 @@ DEFUN(show_lchan_summary,
 
 DEFUN(show_lchan_summary_all,
       show_lchan_summary_all_cmd,
-      "show lchan summary-all [<0-255>] [<0-255>] [<0-7>] [<0-7>]",
+      "show lchan summary-all [" BTS_NR_VTY_ARG_VAL "] [<0-255>] [<0-7>] [<0-7>]",
 	SHOW_STR "Display information about a logical channel\n"
         "Short summary (all lchans)\n"
 	BTS_TRX_TS_LCHAN_STR)
@@ -912,7 +912,7 @@ static int trigger_vamos_mode_modify(struct vty *vty, struct gsm_lchan *lchan, b
 
 DEFUN(handover_subscr_conn,
       handover_subscr_conn_cmd,
-      "bts <0-255> trx <0-255> timeslot <0-7> sub-slot <0-7> handover <0-255>",
+      "bts " BTS_NR_VTY_ARG_VAL " trx <0-255> timeslot <0-7> sub-slot <0-7> handover " BTS_NR_VTY_ARG_VAL,
       BTS_NR_TRX_TS_SS_STR2
       MANUAL_HANDOVER_STR
       "New " BTS_NR_STR)
@@ -922,7 +922,7 @@ DEFUN(handover_subscr_conn,
 
 DEFUN(assignment_subscr_conn,
       assignment_subscr_conn_cmd,
-      "bts <0-255> trx <0-255> timeslot <0-7> sub-slot <0-7> assignment",
+      "bts " BTS_NR_VTY_ARG_VAL " trx <0-255> timeslot <0-7> sub-slot <0-7> assignment",
       BTS_NR_TRX_TS_SS_STR2
       MANUAL_ASSIGNMENT_STR)
 {
@@ -1089,7 +1089,7 @@ static void bts_paging_dump_vty(struct vty *vty, struct gsm_bts *bts)
 
 DEFUN(show_paging,
       show_paging_cmd,
-      "show paging [<0-255>]",
+      "show paging [" BTS_NR_VTY_ARG_VAL "]",
 	SHOW_STR "Display information about paging requests of a BTS\n"
 	BTS_NR_STR)
 {
@@ -1120,7 +1120,7 @@ DEFUN(show_paging,
 
 DEFUN(show_paging_group,
       show_paging_group_cmd,
-      "show paging-group <0-255> IMSI",
+      "show paging-group " BTS_NR_VTY_ARG_VAL " IMSI",
       SHOW_STR "Display the paging group\n"
       BTS_NR_STR "IMSI\n")
 {
@@ -1313,7 +1313,7 @@ DEFUN(drop_bts,
 }
 
 DEFUN(restart_bts, restart_bts_cmd,
-      "restart-bts <0-65535>",
+      "restart-bts " BTS_NR_VTY_ARG_VAL,
       "Restart ip.access nanoBTS through OML\n"
       BTS_NR_STR)
 {
@@ -1352,7 +1352,7 @@ DEFUN(restart_bts, restart_bts_cmd,
 
 DEFUN(bts_resend_sysinfo,
       bts_resend_sysinfo_cmd,
-      "bts <0-255> resend-system-information",
+      "bts " BTS_NR_VTY_ARG_VAL " resend-system-information",
       "BTS Specific Commands\n" BTS_NR_STR
       "Re-generate + re-send BCCH SYSTEM INFORMATION\n")
 {
@@ -1386,7 +1386,7 @@ DEFUN(bts_resend_sysinfo,
 
 DEFUN(bts_resend_power_ctrl_params,
       bts_resend_power_ctrl_params_cmd,
-      "bts <0-255> resend-power-control-defaults",
+      "bts " BTS_NR_VTY_ARG_VAL " resend-power-control-defaults",
       "BTS Specific Commands\n" BTS_NR_STR
       "Re-generate + re-send default MS/BS Power control parameters\n")
 {
@@ -1425,7 +1425,7 @@ DEFUN(bts_resend_power_ctrl_params,
 
 DEFUN(bts_c0_power_red,
       bts_c0_power_red_cmd,
-      "bts <0-255> c0-power-reduction <0-6>",
+      "bts " BTS_NR_VTY_ARG_VAL " c0-power-reduction <0-6>",
       "BTS Specific Commands\n" BTS_NR_STR
       "BCCH carrier power reduction operation\n"
       "Power reduction value (in dB, even numbers only)\n")
@@ -1469,7 +1469,7 @@ DEFUN(bts_c0_power_red,
 /* this command is now hidden, as it's a low-level debug hack, and people should
  * instead use osmo-cbc these days */
 DEFUN_HIDDEN(smscb_cmd, smscb_cmd_cmd,
-	"bts <0-255> smscb-command (normal|schedule|default) <1-4> HEXSTRING",
+	"bts " BTS_NR_VTY_ARG_VAL " smscb-command (normal|schedule|default) <1-4> HEXSTRING",
 	"BTS related commands\n" BTS_NR_STR
 	"SMS Cell Broadcast\n"
 	"Normal (one-shot) SMSCB Message; sent once over Abis+Um\n"
@@ -1539,7 +1539,7 @@ DEFUN_HIDDEN(smscb_cmd, smscb_cmd_cmd,
 }
 
 DEFUN(pdch_act, pdch_act_cmd,
-	"bts <0-255> trx <0-255> timeslot <0-7> pdch (activate|deactivate)",
+	"bts " BTS_NR_VTY_ARG_VAL " trx <0-255> timeslot <0-7> pdch (activate|deactivate)",
 	BTS_NR_TRX_TS_STR2
 	"Packet Data Channel\n"
 	"Activate Dynamic PDCH/TCH (-> PDCH mode)\n"
@@ -1796,7 +1796,7 @@ static int lchan_act_deact(struct vty *vty, const char **argv, int argc)
  * manually in a given mode/codec.  This is useful for receiver
  * performance testing (FER/RBER/...) */
 DEFUN(lchan_act, lchan_act_cmd,
-	"bts <0-255> trx <0-255> timeslot <0-7> (sub-slot|vamos-sub-slot) <0-7> (activate|activate-vamos) (hr|fr|efr|amr|sig) [<0-7>]",
+	"bts " BTS_NR_VTY_ARG_VAL " trx <0-255> timeslot <0-7> (sub-slot|vamos-sub-slot) <0-7> (activate|activate-vamos) (hr|fr|efr|amr|sig) [<0-7>]",
 	BTS_NR_TRX_TS_STR2
 	"Primary sub-slot\n" "VAMOS secondary shadow subslot, range <0-1>, only valid for TCH type timeslots\n"
 	SS_NR_STR
@@ -1808,7 +1808,7 @@ DEFUN(lchan_act, lchan_act_cmd,
 }
 
 DEFUN(lchan_deact, lchan_deact_cmd,
-	"bts <0-255> trx <0-255> timeslot <0-7> (sub-slot|vamos-sub-slot) <0-7> deactivate",
+	"bts " BTS_NR_VTY_ARG_VAL " trx <0-255> timeslot <0-7> (sub-slot|vamos-sub-slot) <0-7> deactivate",
 	BTS_NR_TRX_TS_STR2
 	"Primary sub-slot\n" "VAMOS secondary shadow subslot, range <0-1>, only valid for TCH type timeslots\n"
 	SS_NR_STR
@@ -1855,7 +1855,7 @@ DEFUN_HIDDEN(lchan_act_bts, lchan_act_all_cmd,
 /* Similar to lchan_act, but activates all lchans on the specified BTS at once,
  * this is intended to perform lab tests / measurements. */
 DEFUN_HIDDEN(lchan_act_all_bts, lchan_act_all_bts_cmd,
-	     "bts <0-255> (activate-all-lchan|deactivate-all-lchan)",
+	     "bts " BTS_NR_VTY_ARG_VAL " (activate-all-lchan|deactivate-all-lchan)",
 	     "BTS Specific Commands\n" BTS_NR_STR
 	     ACTIVATE_ALL_LCHANS_STR
 	     DEACTIVATE_ALL_LCHANS_STR)
@@ -1895,7 +1895,7 @@ DEFUN_HIDDEN(lchan_act_all_bts, lchan_act_all_bts_cmd,
 /* Similar to lchan_act, but activates all lchans on the specified BTS at once,
  * this is intended to perform lab tests / measurements. */
 DEFUN_HIDDEN(lchan_act_all_trx, lchan_act_all_trx_cmd,
-	     "bts <0-255> trx <0-255> (activate-all-lchan|deactivate-all-lchan)",
+	     "bts " BTS_NR_VTY_ARG_VAL " trx <0-255> (activate-all-lchan|deactivate-all-lchan)",
 	     "BTS for manual command\n" BTS_NR_STR
 	     "TRX for manual command\n" TRX_NR_STR
 	     ACTIVATE_ALL_LCHANS_STR
@@ -1937,7 +1937,7 @@ DEFUN_HIDDEN(lchan_act_all_trx, lchan_act_all_trx_cmd,
 }
 
 DEFUN(lchan_set_mspower, lchan_set_mspower_cmd,
-      "bts <0-255> trx <0-255> timeslot <0-7> sub-slot <0-7> ms-power <0-40> [verify]\n",
+      "bts " BTS_NR_VTY_ARG_VAL " trx <0-255> timeslot <0-7> sub-slot <0-7> ms-power <0-40> [verify]\n",
       BTS_NR_TRX_TS_SS_STR2
       "Manually force MS Uplink Power Level in dBm on the lchan (for testing)\n"
       "Set transmit power of the MS in dBm\n"
@@ -1988,7 +1988,7 @@ DEFUN(lchan_set_mspower, lchan_set_mspower_cmd,
 }
 
 DEFUN(vamos_modify_lchan, vamos_modify_lchan_cmd,
-      "bts <0-255> trx <0-255> timeslot <0-7> sub-slot <0-7> modify (vamos|non-vamos) " TSC_ARGS_OPT,
+      "bts " BTS_NR_VTY_ARG_VAL " trx <0-255> timeslot <0-7> sub-slot <0-7> modify (vamos|non-vamos) " TSC_ARGS_OPT,
       BTS_NR_TRX_TS_SS_STR2
       "Manually send Channel Mode Modify (for debugging)\n"
       "Enable VAMOS channel mode\n" "Disable VAMOS channel mode\n"
@@ -2026,7 +2026,7 @@ DEFUN(vamos_modify_lchan, vamos_modify_lchan_cmd,
 /* Debug command to send lchans from state LCHAN_ST_UNUSED to state
  * LCHAN_ST_BORKEN and vice versa. */
 DEFUN_HIDDEN(lchan_set_borken, lchan_set_borken_cmd,
-	     "bts <0-255> trx <0-255> timeslot <0-7> sub-slot <0-7> (borken|unused)",
+	     "bts " BTS_NR_VTY_ARG_VAL " trx <0-255> timeslot <0-7> sub-slot <0-7> (borken|unused)",
 	     BTS_NR_TRX_TS_SS_STR2
 	     "send lchan to state LCHAN_ST_BORKEN (for debugging)\n"
 	     "send lchan to state LCHAN_ST_UNUSED (for debugging)\n")
@@ -2071,7 +2071,7 @@ DEFUN_HIDDEN(lchan_set_borken, lchan_set_borken_cmd,
 }
 
 DEFUN(lchan_mdcx, lchan_mdcx_cmd,
-	"bts <0-255> trx <0-255> timeslot <0-7> sub-slot <0-7> mdcx A.B.C.D <0-65535>",
+	"bts " BTS_NR_VTY_ARG_VAL " trx <0-255> timeslot <0-7> sub-slot <0-7> mdcx A.B.C.D <0-65535>",
 	BTS_NR_TRX_TS_SS_STR2
 	"Modify RTP Connection\n" "MGW IP Address\n" "MGW UDP Port\n")
 {
@@ -2109,7 +2109,7 @@ DEFUN(lchan_mdcx, lchan_mdcx_cmd,
 }
 
 DEFUN(lchan_reassign, lchan_reassign_cmd,
-	"bts <0-255> trx <0-255> timeslot <0-7> (sub-slot|vamos-sub-slot) <0-7> "
+	"bts " BTS_NR_VTY_ARG_VAL " trx <0-255> timeslot <0-7> (sub-slot|vamos-sub-slot) <0-7> "
 	"reassign-to trx <0-255> timeslot <0-7> (sub-slot|vamos-sub-slot) <0-7> "
 	TSC_ARGS_OPT,
 	BTS_NR_TRX_TS_STR2
@@ -3219,7 +3219,7 @@ DEFUN_ATTR(cfg_bsc_bts_ramping_step_size,
 
 DEFUN(bts_unblock_setup_ramping,
       bts_unblock_setup_ramping_cmd,
-      "bts <0-255> unblock-setup-ramping",
+      "bts " BTS_NR_VTY_ARG_VAL " unblock-setup-ramping",
       "BTS Specific Commands\n" BTS_NR_STR
       "Unblock and allow to configure a BTS if kept back by BTS ramping\n")
 {
@@ -3325,7 +3325,7 @@ DEFUN(show_pos,
 
 DEFUN(gen_position_trap,
       gen_position_trap_cmd,
-      "generate-location-state-trap <0-255>",
+      "generate-location-state-trap " BTS_NR_VTY_ARG_VAL,
       "Generate location state report\n"
       "BTS to report\n")
 {

src/osmo-bsc/bts_vty.c
@@ -106,7 +106,7 @@ static struct cmd_node power_ctrl_node = {
 /* per-BTS configuration */
 DEFUN_ATTR(cfg_bts,
 	   cfg_bts_cmd,
-	   "bts <0-255>",
+	   "bts " BTS_NR_VTY_ARG_VAL,
 	   "Select a BTS to configure\n"
 	   BTS_NR_STR,
 	   CMD_ATTR_IMMEDIATE)
@@ -2605,7 +2605,7 @@ DEFUN_USRATTR(cfg_bts_codec4,
 }
 
 DEFUN_ATTR(cfg_bts_depends_on, cfg_bts_depends_on_cmd,
-	   "depends-on-bts <0-255>",
+	   "depends-on-bts " BTS_NR_VTY_ARG_VAL,
 	   "This BTS can only be started if another one is up\n"
 	   BTS_NR_STR, CMD_ATTR_IMMEDIATE)
 {
@@ -2639,7 +2639,7 @@ DEFUN_ATTR(cfg_bts_depends_on, cfg_bts_depends_on_cmd,
 }
 
 DEFUN_ATTR(cfg_bts_no_depends_on, cfg_bts_no_depends_on_cmd,
-	   "no depends-on-bts <0-255>",
+	   "no depends-on-bts " BTS_NR_VTY_ARG_VAL,
 	   NO_STR "This BTS can only be started if another one is up\n"
 	   BTS_NR_STR, CMD_ATTR_IMMEDIATE)
 {

src/osmo-bsc/gsm_data.c
@@ -49,6 +49,8 @@
 
 void *tall_bsc_ctx = NULL;
 
+osmo_static_assert(BTS_NR_MAX == ((2 << ((sizeof(gsm_bts_nr_t) * 8) - 1)) - 1), _gsm_bts_nr_t_size);
+
 void set_ts_e1link(struct gsm_bts_trx_ts *ts, uint8_t e1_nr,
 		   uint8_t e1_ts, uint8_t e1_ts_ss)
 {
@@ -250,7 +252,7 @@ const char *gsm_chreq_name(enum gsm_chreq_reason_t c)
 	return get_value_string(chreq_names, c);
 }
 
-struct gsm_bts *gsm_bts_num(const struct gsm_network *net, int num)
+struct gsm_bts *gsm_bts_num(const struct gsm_network *net, gsm_bts_nr_t num)
 {
 	struct gsm_bts *bts;
 

commit 887ad810b05474b3e12cc4227dcccfb1bd6a8b14

Fix BTS nr >255 wrapping up during bts allocation
Commit which introduced the gsm_bts_nr_t type forgot to use it in these
functions.
As a result, bts 256 ended up trying to allocate rate counters for id=0.

Change-Id: I5128becaf453f5cfbc722a55c756432e525fc884
Fixes: 6fe0e28a23a3aa3e707d147569add7f68d5dc4a1
Related: SYS#7063

include/osmocom/bsc/bts.h
@@ -805,7 +805,7 @@ static inline const struct osmo_location_area_id *bts_lai(struct gsm_bts *bts)
 	return &lai;
 }
 
-struct gsm_bts *gsm_bts_alloc(struct gsm_network *net, struct gsm_bts_sm *bts_sm, uint8_t bts_num);
+struct gsm_bts *gsm_bts_alloc(struct gsm_network *net, struct gsm_bts_sm *bts_sm, gsm_bts_nr_t bts_num);
 int gsm_bts_check_cfg(struct gsm_bts *bts);
 
 char *gsm_bts_name(const struct gsm_bts *bts);

include/osmocom/bsc/bts_sm.h
@@ -68,7 +68,7 @@ static inline struct gsm_bts *gsm_bts_sm_get_bts(struct gsm_bts_sm *site_mgr) {
 	return site_mgr->bts[0];
 }
 
-struct gsm_bts_sm *gsm_bts_sm_alloc(struct gsm_network *net, uint8_t bts_num);
+struct gsm_bts_sm *gsm_bts_sm_alloc(struct gsm_network *net, gsm_bts_nr_t bts_num);
 
 void gsm_bts_sm_mo_reset(struct gsm_bts_sm *bts_sm);
 

src/osmo-bsc/bts.c
@@ -193,7 +193,7 @@ static int gsm_bts_talloc_destructor(struct gsm_bts *bts)
  * This part is shared among the thin programs in osmo-bsc/src/utils/.
  * osmo-bsc requires further initialization that pulls in more dependencies (see
  * bsc_bts_alloc_register()). */
-struct gsm_bts *gsm_bts_alloc(struct gsm_network *net, struct gsm_bts_sm *bts_sm, uint8_t bts_num)
+struct gsm_bts *gsm_bts_alloc(struct gsm_network *net, struct gsm_bts_sm *bts_sm, gsm_bts_nr_t bts_num)
 {
 	struct gsm_bts *bts = talloc_zero(bts_sm, struct gsm_bts);
 

src/osmo-bsc/bts_sm.c
@@ -49,7 +49,7 @@ static int gsm_bts_sm_talloc_destructor(struct gsm_bts_sm *bts_sm)
 	return 0;
 }
 
-struct gsm_bts_sm *gsm_bts_sm_alloc(struct gsm_network *net, uint8_t bts_num)
+struct gsm_bts_sm *gsm_bts_sm_alloc(struct gsm_network *net, gsm_bts_nr_t bts_num)
 {
 	struct gsm_bts_sm *bts_sm = talloc_zero(net, struct gsm_bts_sm);
 	struct gsm_bts *bts;