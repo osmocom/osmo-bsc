osmocom/osmo-bsc

commit 15284337ece94e3ff3ebf9575096290f67d69a88

Introduce hashtable to lookup bts by <LAC,CI>
This is useful to quickly lookup pagings aiming at a LAC+CI.
Until now, we first looked up in the LAC hashtable and then iterated
over it. However, that means a potential configuration where all BTS
share the same LAC will end up in iterating over 256 bts, which is not
nice.

Change-Id: I47db6c7543e5c6c3b8f0de3ae5ee1b53c2b5f16f
Related: SYS#7062

include/osmocom/bsc/bts.h
@@ -345,6 +345,8 @@ struct gsm_bts {
 	struct hlist_node node_by_nr;
 	/*! Entry in hash table network->bts_by_lac. */
 	struct hlist_node node_by_lac;
+	/*! Entry in hash table network->bts_by_lac_ci. */
+	struct hlist_node node_by_lac_ci;
 	/*! Entry in hash table network->bts_by_ci. */
 	struct hlist_node node_by_ci;
 

src/osmo-bsc/bts.c
@@ -161,6 +161,7 @@ static int gsm_bts_talloc_destructor(struct gsm_bts *bts)
 	llist_del(&bts->list);
 	hash_del(&bts->node_by_nr);
 	hash_del(&bts->node_by_lac);
+	hash_del(&bts->node_by_lac_ci);
 	hash_del(&bts->node_by_ci);
 
 	paging_destructor(bts);
@@ -209,6 +210,7 @@ struct gsm_bts *gsm_bts_alloc(struct gsm_network *net, struct gsm_bts_sm *bts_sm
 
 	/* Default bts->location_area_code == GSM_LAC_RESERVED_DETACHED, don't add to hashtable: */
 	INIT_HLIST_NODE(&bts->node_by_lac);
+	INIT_HLIST_NODE(&bts->node_by_lac_ci);
 	/* Default CI = 0: */
 	hash_add(net->bts_by_ci, &bts->node_by_ci, bts->cell_identity);
 

src/osmo-bsc/bts_vty.c
@@ -271,6 +271,11 @@ DEFUN_USRATTR(cfg_bts_ci,
 	bts->cell_identity = ci;
 	hash_del(&bts->node_by_ci);
 	hash_add(bts->network->bts_by_ci, &bts->node_by_ci, bts->cell_identity);
+	if (bts->location_area_code != GSM_LAC_RESERVED_DETACHED) {
+		hash_del(&bts->node_by_lac_ci);
+		hash_add(bts->network->bts_by_lac_ci, &bts->node_by_lac_ci,
+			 LAC_CI_HASHTABLE_KEY(bts->location_area_code, bts->cell_identity));
+	}
 
 	return CMD_SUCCESS;
 }
@@ -296,7 +301,10 @@ DEFUN_USRATTR(cfg_bts_lac,
 
 	bts->location_area_code = lac;
 	hash_del(&bts->node_by_lac);
+	hash_del(&bts->node_by_lac_ci);
 	hash_add(bts->network->bts_by_lac, &bts->node_by_lac, bts->location_area_code);
+	hash_add(bts->network->bts_by_lac_ci, &bts->node_by_lac_ci,
+		 LAC_CI_HASHTABLE_KEY(bts->location_area_code, bts->cell_identity));
 
 	return CMD_SUCCESS;
 }

include/osmocom/bsc/gsm_data.h
@@ -989,6 +989,8 @@ struct gsm_network {
 	struct llist_head bts_rejected;
 	DECLARE_HASHTABLE(bts_by_nr, 10);
 	DECLARE_HASHTABLE(bts_by_lac, 10);
+	DECLARE_HASHTABLE(bts_by_lac_ci, 10);
+#define LAC_CI_HASHTABLE_KEY(lac, ci) ((((uint32_t)(ci)) << sizeof(lac)) | (uint32_t)(lac))
 	DECLARE_HASHTABLE(bts_by_ci, 10);
 
 	/* BTS-based counters when we can't find the actual BTS

src/osmo-bsc/neighbor_ident.c
@@ -377,7 +377,8 @@ int neighbor_address_resolution(const struct gsm_network *net, const struct cell
 	struct gsm_bts *local_neighbor = NULL;
 	struct gsm0808_cell_id_list2 remote_neighbors = { 0 };
 
-	hash_for_each_possible(net->bts_by_lac, bts_tmp, node_by_lac, lac) {
+	hash_for_each_possible(net->bts_by_lac_ci, bts_tmp, node_by_lac_ci,
+			       LAC_CI_HASHTABLE_KEY(lac, cell_id)) {
 		if (bts_tmp->location_area_code != lac)
 			continue;
 		if (bts_tmp->cell_identity != cell_id)

src/osmo-bsc/osmo_bsc_bssap.c
@@ -150,7 +150,8 @@ static void page_cgi(const struct bsc_paging_params *params)
 		if (!osmo_plmn_cmp(&id->lai.plmn, &bsc_gsmnet->plmn)) {
 			int paged = 0;
 			struct gsm_bts *bts;
-			hash_for_each_possible(bsc_gsmnet->bts_by_lac, bts, node_by_lac, id->lai.lac) {
+			hash_for_each_possible(bsc_gsmnet->bts_by_lac_ci, bts, node_by_lac_ci,
+					       LAC_CI_HASHTABLE_KEY(id->lai.lac, id->cell_identity)) {
 				if (bts->location_area_code != id->lai.lac)
 					continue;
 				if (bts->cell_identity != id->cell_identity)
@@ -179,7 +180,8 @@ static void page_lac_and_ci(const struct bsc_paging_params *params)
 		const struct osmo_lac_and_ci_id *id = &params->cil.id_list[i].lac_and_ci;
 		int paged = 0;
 		struct gsm_bts *bts;
-		hash_for_each_possible(bsc_gsmnet->bts_by_lac, bts, node_by_lac, id->lac) {
+		hash_for_each_possible(bsc_gsmnet->bts_by_lac_ci, bts, node_by_lac_ci,
+				       LAC_CI_HASHTABLE_KEY(id->lac, id->ci)) {
 			if (bts->location_area_code != id->lac)
 				continue;
 			if (bts->cell_identity != id->ci)

commit e6b593f3a9dee85deb8682a7cfea0134db8d3982

Fix missing LAC/CI hashtable update upon CTRL SET
Add functions to set LAC/CI on a BTS and update hashtables

Use these function from both the vty and the control interface.

This commit fixes the issue where an update to LAC or CI
via the control interface was not resulting in updated
hashtables and therefore the BTS was not found during paging.

Related: OS#6840
Fixes: 15284337ece94e3ff3ebf9575096290f67d69a88

Change-Id: If9509534bd4a75f8ed8fb3e0a6bec701c7596861

include/osmocom/bsc/bts.h
@@ -825,6 +825,9 @@ struct gsm_lchan *gsm_bts_get_cbch(struct gsm_bts *bts);
 int gsm_set_bts_model(struct gsm_bts *bts, struct gsm_bts_model *model);
 int gsm_set_bts_type(struct gsm_bts *bts, enum gsm_bts_type type);
 
+void gsm_bts_set_lac(struct gsm_bts *bts, uint16_t lac);
+void gsm_bts_set_ci(struct gsm_bts *bts, uint16_t ci);
+
 struct gsm_bts_trx *gsm_bts_trx_num(const struct gsm_bts *bts, int num);
 
 int bts_gprs_mode_is_compat(struct gsm_bts *bts, enum bts_gprs_mode mode);

src/osmo-bsc/bts.c
@@ -758,6 +758,30 @@ int gsm_set_bts_type(struct gsm_bts *bts, enum gsm_bts_type type)
 	return 0;
 }
 
+void gsm_bts_set_lac(struct gsm_bts *bts, uint16_t lac)
+{
+	bts->location_area_code = lac;
+
+	hash_del(&bts->node_by_lac);
+	hash_del(&bts->node_by_lac_ci);
+	hash_add(bts->network->bts_by_lac, &bts->node_by_lac, bts->location_area_code);
+	hash_add(bts->network->bts_by_lac_ci, &bts->node_by_lac_ci,
+		 LAC_CI_HASHTABLE_KEY(bts->location_area_code, bts->cell_identity));
+}
+
+void gsm_bts_set_ci(struct gsm_bts *bts, uint16_t ci)
+{
+	bts->cell_identity = ci;
+
+	hash_del(&bts->node_by_ci);
+	hash_add(bts->network->bts_by_ci, &bts->node_by_ci, bts->cell_identity);
+	if (bts->location_area_code != GSM_LAC_RESERVED_DETACHED) {
+		hash_del(&bts->node_by_lac_ci);
+		hash_add(bts->network->bts_by_lac_ci, &bts->node_by_lac_ci,
+			 LAC_CI_HASHTABLE_KEY(bts->location_area_code, bts->cell_identity));
+	}
+}
+
 int bts_gprs_mode_is_compat(struct gsm_bts *bts, enum bts_gprs_mode mode)
 {
 	if (mode != BTS_GPRS_NONE &&

src/osmo-bsc/bts_ctrl.c
@@ -21,6 +21,7 @@
 
 #include <errno.h>
 #include <time.h>
+#include <inttypes.h>
 
 #include <osmocom/ctrl/control_cmd.h>
 
@@ -244,9 +245,76 @@ err:
 	return 1;
 }
 
+static int get_bts_lac(struct ctrl_cmd *cmd, void *data)
+{
+	struct gsm_bts *bts = cmd->node;
+	cmd->reply = talloc_asprintf(cmd, "%" PRIu16, bts->location_area_code);
+	if (!cmd->reply) {
+		cmd->reply = "OOM";
+		return CTRL_CMD_ERROR;
+	}
+	return CTRL_CMD_REPLY;
+}
+
+static int set_bts_lac(struct ctrl_cmd *cmd, void *data)
+{
+	struct gsm_bts *bts = cmd->node;
+	int lac;
+
+	osmo_str_to_int(&lac, cmd->value, 0, 0, 0xffff);
+	gsm_bts_set_lac(bts, lac);
+	return get_bts_lac(cmd, data);
+}
+
+static int verify_bts_lac(struct ctrl_cmd *cmd, const char *value, void *data)
+{
+	int lac;
+
+	if (osmo_str_to_int(&lac, cmd->value, 0, 0, 0xffff) < 0) {
+		cmd->reply = "Input not within the range";
+		return -1;
+	}
+	if (lac == GSM_LAC_RESERVED_DETACHED || lac == GSM_LAC_RESERVED_ALL_BTS) {
+		cmd->reply = talloc_asprintf(cmd, "LAC %d is reserved by GSM 04.08", lac);
+		return -1;
+	}
+	return 0;
+}
+
+static int get_bts_ci(struct ctrl_cmd *cmd, void *data)
+{
+	struct gsm_bts *bts = cmd->node;
+	cmd->reply = talloc_asprintf(cmd, "%" PRIu16, bts->cell_identity);
+	if (!cmd->reply) {
+		cmd->reply = "OOM";
+		return CTRL_CMD_ERROR;
+	}
+	return CTRL_CMD_REPLY;
+}
+
+static int set_bts_ci(struct ctrl_cmd *cmd, void *data)
+{
+	struct gsm_bts *bts = cmd->node;
+	int ci;
+
+	osmo_str_to_int(&ci, cmd->value, 0, 0, 0xffff);
+	gsm_bts_set_ci(bts, ci);
+	return get_bts_ci(cmd, data);
+}
+
+static int verify_bts_ci(struct ctrl_cmd *cmd, const char *value, void *data)
+{
+	int ci;
+
+	if (osmo_str_to_int(&ci, cmd->value, 0, 0, 0xffff) < 0) {
+		cmd->reply = "Input not within the range";
+		return -1;
+	}
+	return 0;
+}
 /* BTS related commands below */
-CTRL_CMD_DEFINE_RANGE(bts_lac, "location-area-code", struct gsm_bts, location_area_code, 0, 65535);
-CTRL_CMD_DEFINE_RANGE(bts_ci, "cell-identity", struct gsm_bts, cell_identity, 0, 65535);
+CTRL_CMD_DEFINE(bts_lac, "location-area-code");
+CTRL_CMD_DEFINE(bts_ci, "cell-identity");
 CTRL_CMD_DEFINE_RANGE(bts_bsic, "bsic", struct gsm_bts, bsic, 0, 63);
 CTRL_CMD_DEFINE_RANGE(bts_rach_max_delay, "rach-max-delay", struct gsm_bts, rach_max_delay, 1, 127);
 CTRL_CMD_DEFINE_RANGE(bts_rach_expiry_timeout, "rach-expiry-timeout", struct gsm_bts, rach_expiry_timeout, 4, 64);

src/osmo-bsc/bts_vty.c
@@ -274,15 +274,8 @@ DEFUN_USRATTR(cfg_bts_ci,
 			ci, VTY_NEWLINE);
 		return CMD_WARNING;
 	}
-	bts->cell_identity = ci;
-	hash_del(&bts->node_by_ci);
-	hash_add(bts->network->bts_by_ci, &bts->node_by_ci, bts->cell_identity);
-	if (bts->location_area_code != GSM_LAC_RESERVED_DETACHED) {
-		hash_del(&bts->node_by_lac_ci);
-		hash_add(bts->network->bts_by_lac_ci, &bts->node_by_lac_ci,
-			 LAC_CI_HASHTABLE_KEY(bts->location_area_code, bts->cell_identity));
-	}
 
+	gsm_bts_set_ci(bts, ci);
 	return CMD_SUCCESS;
 }
 
@@ -305,13 +298,7 @@ DEFUN_USRATTR(cfg_bts_lac,
 		return CMD_WARNING;
 	}
 
-	bts->location_area_code = lac;
-	hash_del(&bts->node_by_lac);
-	hash_del(&bts->node_by_lac_ci);
-	hash_add(bts->network->bts_by_lac, &bts->node_by_lac, bts->location_area_code);
-	hash_add(bts->network->bts_by_lac_ci, &bts->node_by_lac_ci,
-		 LAC_CI_HASHTABLE_KEY(bts->location_area_code, bts->cell_identity));
-
+	gsm_bts_set_lac(bts, lac);
 	return CMD_SUCCESS;
 }
 